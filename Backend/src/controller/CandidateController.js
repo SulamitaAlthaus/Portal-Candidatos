52f3a30709ed8c6cec1239637f61620bbd6d3a5a
Sulamita Althaus <40037816+SulamitaAlthaus@users.noreply.github.com>
2021-02-01 13:37:38 -0300

    criado funções de update, find e delete

--- Backend/src/controller/CandidateController.js
+++ Backend/src/controller/CandidateController.js
@@ -14,6 +14,33 @@ class CandidateController {
                 return res.status(500).json(error);
             })
     }
+    async update(req, res){
+        await CandidateModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true})
+        .then(response =>{
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        })
+    }
+    async all(req, res){
+        await CandidateModel.find({})
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+    }
+    async delete(req, res){
+        await CandidateModel.deleteOne({'_id': req.params.id})
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error)
+        });
+    }
 }
 
 module.exports = new CandidateController();
--- Backend/src/routes/CandidateRoutes.js
+++ Backend/src/routes/CandidateRoutes.js
@@ -5,5 +5,9 @@ const CandidateController = require('../controller/CandidateController');
 const CandValidation = require('../middlewares/CandValidation');
 
 router.post('/', CandValidation, CandidateController.create);
+router.put('/:id', CandidateController.update);
+router.get('/filter/all', CandidateController.all);
+router.delete('/:id', CandidateController.delete);
+
 
 module.exports = router;
