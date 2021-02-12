7dcc627483f4a67553a7c310c0eaaf5eb15c331e
Sulamita Althaus <40037816+SulamitaAlthaus@users.noreply.github.com>
2021-02-12 17:56:10 -0300

    Ajustado linkedin

--- Frontend/portal-candidatos/src/components/ListCandidate/index.js
+++ Frontend/portal-candidatos/src/components/ListCandidate/index.js
@@ -22,7 +22,7 @@ function ListCandidate({ id, nome, email, idade, url, tecnologias }) {
             <td><S.Row>{nome}</S.Row></td>
             <td><S.Row>{email}</S.Row></td>
             <td><S.Row>{idade}</S.Row></td>
-            <td><S.Row><a href={("https://www.linkedin.com/in/"+{url}+"/")}>{url}</a></S.Row></td>
+            <td><S.Row><a href={("https://www.linkedin.com/in/"+url.toString()+"/")}>{url}</a></S.Row></td>
             <td><S.Row>{tecnologias.join(', ')}</S.Row></td>
             <td><S.Row><S.Button onClick={deleteCandidate}>Delete</S.Button>
             <S.Button onClick={() => setIsModalVisible(true)}>Editar</S.Button></S.Row></td>
